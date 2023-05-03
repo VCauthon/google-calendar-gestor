c7903292ceb2120446203a399417aaf09406a821
Victor <victormasmon@gmail.com>
2023-05-03 07:24:33 +0200

    Update main.py

---
+++ src/main.py
@@ -0,0 +1,19 @@
+Here are some example commands that a Python CLI script working with the Google API might include, including the "create event" command:
+
+1. Authentication command: This command allows the user to authenticate with the Google API using their credentials.
+
+2. List calendars command: This command lists all the calendars available in the user's account.
+
+3. Create event command: This command allows the user to create a new event on a specified calendar.
+
+4. List events command: This command lists all the events on a specified calendar.
+
+5. Update event command: This command allows the user to update an existing event on a specified calendar.
+
+6. Delete event command: This command allows the user to delete an existing event on a specified calendar.
+
+7. Search events command: This command allows the user to search for events on a specified calendar using specific criteria, such as date range, keyword, or event type.
+
+ 8. create event based on gpt prom
+  
+These commands can be implemented using the appropriate methods and endpoints from the Google API Python client library.
